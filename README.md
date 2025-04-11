256c9b4f204338051eb57be6cdd31697b8439d2a
Yayaka <yayaka@gmx.fr>
2025-04-11 09:28:16 +0200

    feat: completed basic app functionalities

--- README.md
+++ README.md
@@ -3,4 +3,29 @@ A simple CLI program to fetch the recent activity of a GitHub user, built in Pyt
 
 For more info: https://roadmap.sh/projects/github-user-activity
 
-WIP
+Usage: 'github-activity.py [-h | --help] [-e | --events] [<username> [-f <filter>] [-l <limit>]]'
+
+'''
+# show a list of commands
+github-activity.py --help
+
+# fetch github activity for a user
+github-activity.py <username>
+
+# filter results by type
+github-activity.py <username> -f <filter>
+
+# list all event types
+github-activity.py -e
+
+# limit output size
+github-activity.py <username> -l <limit>
+
+# show yyka's last 3 commits
+github-activity.py yyka -l 3 -f PushEvent
+'''
+
+<details>
+<summary>Misc</summary>
+Total time taken: 10h
+</details>
---
+++ constants.py
@@ -0,0 +1,23 @@
+EVENTS = {
+    "CommitCommentEvent": "A commit comment is created",
+    "CreateEvent": "A Git branch or tag is created",
+    "DeleteEvent": "A Git branch or tag is deleted",
+    "ForkEvent": "A user forks a repository",
+    "GollumEvent": "A wiki page is created or updated",
+    "IssueCommentEvent": "Activity related to an issue "
+    "or pull request comment",
+    "IssuesEvent": "Activity related to an issue",
+    "MemberEvent": "Activity related to repository collaborators",
+    "PublicEvent": "When a private repository is made public",
+    "PullRequestEvent": "Activity related to pull requests",
+    "PullRequestReviewEvent": "Activity related to pull request reviews",
+    "PullRequestReviewCommentEvent": "Activity related to pull "
+    "request review comments in the pull request's unified diff",
+    "PullRequestReviewThreadEvent": "Activity related to a comment "
+    "thread on a pull request being marked as resolved or unresolved",
+    "PushEvent": "One or more commits are pushed to "
+    "a repository branch or tag",
+    "ReleaseEvent": "Activity related to a release",
+    "SponsorshipEvent": "Activity related to a sponsorship listing",
+    "WatchEvent": "When someone stars a repository"
+    }
